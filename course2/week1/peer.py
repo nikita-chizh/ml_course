a5b2a7b3fd76cfccd7bf4b9510918dceeb57df3c
nikita <nikita1993chizh@gmail.com>
2018-03-26 23:55:05 +0300

    Почти доделал peer-rewiew, осталось 2 пункта

--- course2/week1/peer.py
+++ course2/week1/peer.py
@@ -36,12 +36,14 @@ def weight_category(weight):
         return 3
     return 2
 
+
 data['weight_cat'] = data['Weight'].apply(weight_category)
-sns.boxplot(x = data['weight_cat'], y = data['Height'])
+sns.boxplot(x=data['weight_cat'], y=data['Height'])
 plt.show()
 
 # SubTask 5 scatter plot зависимости роста от веса
 data.plot.scatter(y='Height', x='Weight', color='red', title='Height(Weight)', s=1)
+
 
 # Task 2
 
@@ -65,6 +67,7 @@ def err(data_frame, w0, w1, y_col='Height', x_col='Weight'):
 def line(x, w0, w1):
     return w0 + w1 * x
 
+
 def draw_lines_and_scatter(scat_df, X, lines, patches):
     data.plot.scatter(y='Height', x='Weight', color='red', title='Height(Weight)', s=1)
     for line in lines:
@@ -72,36 +75,41 @@ def draw_lines_and_scatter(scat_df, X, lines, patches):
     plt.legend(handles=patches)
     plt.show()
 
-#Draw lines on scatter
+
+# Draw lines on scatter
 lines = []
 patches = []
 weight_b = (80, 200)
 X = np.linspace(weight_b[0], weight_b[1])
-lines.append(([line(x, weight_b[0], 0.05) for x in X],"green"))
+lines.append(([line(x, weight_b[0], 0.05) for x in X], "green"))
 patches.append(mpatches.Patch(color='green', label='w0=60, w1=0.05'))
 lines.append(([line(x, 50, 0.16) for x in X], "blue"))
 patches.append(mpatches.Patch(color='blue', label='w0=50, w1=0.16'))
 draw_lines_and_scatter(data, X, lines, patches)
 
 # SubTask 8
-err_w1  = lambda w1: err(data, 50, w1)
+err_w1 = lambda w1: err(data, 50, w1)
 W1 = np.linspace(-10, 10, 30)
 plt.xlabel('W1', fontsize=15)
 plt.ylabel('error', fontsize=15)
-plt.plot(W1, [err_w1(w1) for w1 in W1], lw=5, alpha=0.6,  color='blue')
+plt.plot(W1, [err_w1(w1) for w1 in W1], lw=5, alpha=0.6, color='blue')
 plt.show()
 
 # SubTask 9
 w1_opt = opt.minimize_scalar(err_w1)
 print("W1 OPTIMAL=", w1_opt.x)
-#optimal w1 line
+# optimal w1 line
 lines.append(([line(x, 50, w1_opt.x) for x in X], "yellow"))
-patches.append(mpatches.Patch(color='yellow', label= "w0=50" + ' Opt w1=' + str(w1_opt.x)))
+patches.append(mpatches.Patch(color='yellow', label="w0=50" + ' Opt w1=' + str(w1_opt.x)))
 draw_lines_and_scatter(data, X, lines, patches)
 
 # SubTask 10
 from mpl_toolkits.mplot3d import Axes3D
+
+
 def err_w0_w1(w0_, w1_): return err(data, w0_, w1_)
+
+
 fig = plt.figure()
 ax = fig.gca(projection='3d')  # get current axis
 size = 10
@@ -118,14 +126,18 @@ plt.show()
 
 # SubTask 11
 from scipy.optimize import minimize
+
+
 def error_for_opt(W):
     return err(data, W[0], W[1])
-x0 = np.array([0,0])
+
+
+x0 = np.array([0, 0])
 res = minimize(error_for_opt, x0, method='L-BFGS-B')
 print("W0,W1 OPTIMAL=", res.x)
-#draw what we had
+# draw what we had
 X = np.linspace(weight_b[0], weight_b[1])
 lines.append(([line(x, res.x[0], res.x[1]) for x in X], "black"))
 patches.append(mpatches.Patch(color='black', label='Opt w0=' + str(res.x[0]) +
-                                                    ' Opt w1=' + str(res.x[1])))
+                                                   ' Opt w1=' + str(res.x[1])))
 draw_lines_and_scatter(data, X, lines, patches)
--- course2/week2/review.py
+++ course2/week2/review.py
@@ -3,17 +3,49 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-path = "/home/nikita/Desktop/some/help/bl/course/1/2.1/2_course/week2/"
+path = "/Users/nikita/PycharmProjects/ML_Tasks/course2/week2/"
 df = pd.read_csv(path + "bikes_rent.csv")
-print(df.head())
 #
 fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(15, 10))
 a = 0 / 4
 for idx, feature in enumerate(df.columns[:-1]):
     df.plot(feature, "cnt", subplots=True, kind="scatter", ax=axes[int(idx / 4), int(idx % 4)])
-plt.show()
 # __Блок 1. Ответьте на вопросы (каждый 0.5 балла):__
 # 1. Каков характер зависимости числа прокатов от месяца?
 #   Парабола с отрицательным коф у квадрата
 # 1. Укажите один или два признака, от которых число прокатов скорее всего зависит линейно
 #    температура и ощущаемая температура
+df1 = df.loc[:, df.columns != "cnt"]
+df2 = df["cnt"]
+# print(df1.corr())
+# print(df1.corrwith(df2))
+#
+df3 = df.loc[:, "temp":"cnt"]
+from sklearn.preprocessing import scale
+from sklearn.utils import shuffle
+df_shuffled = shuffle(df, random_state=123)
+X = scale(df_shuffled[df_shuffled.columns[:-1]])
+y = df_shuffled["cnt"]
+from sklearn.linear_model import LinearRegression
+
+lregr  = LinearRegression()
+lregr.fit(X, y)
+wghts_feat  = zip(lregr.coef_, df.columns)
+#
+# Код 3.1 (1 балл)
+alphas = np.arange(1, 500, 50)
+coefs_lasso = np.zeros((alphas.shape[0], X.shape[1]))  # матрица весов размера (число регрессоров) x (число признаков)
+coefs_ridge = np.zeros((alphas.shape[0], X.shape[1]))
+# Для каждого значения коэффициента из alphas обучите регрессор Lasso
+# и запишите веса в соответствующую строку матрицы coefs_lasso (вспомните встроенную в python функцию enumerate),
+# а затем обучите Ridge и запишите веса в coefs_ridge.
+from sklearn.linear_model import Lasso, Ridge
+
+for i in range(0, alphas.shape[0]):
+    lasso = Lasso(alpha=alphas[i])
+    lasso.fit(X, y)
+    coefs_lasso[i] = lasso.coef_
+    #
+    ridge = Ridge(alpha=alphas[i])
+    ridge.fit(X, y)
+    coefs_ridge[i] = ridge.coef_
