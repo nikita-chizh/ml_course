c06d44bff2affdb7b7cbd61b9a6103e6feee51ad
nikita <nikita1993chizh@gmail.com>
2018-05-21 08:15:17 +0300

    написал функцию для 1й недели

--- course6/week1/data_preparing.py
+++ course6/week1/data_preparing.py
@@ -24,7 +24,7 @@ def prepare_train_set(path_to_csv_files, session_length=10):
     pass
 
 
-path = "/Users/nikita/PycharmProjects/ML_Tasks/course6/capstone_user_identification/3users"
+uspath = "/Users/nikita/PycharmProjects/ML_Tasks/course6/capstone_user_identification/3users/"
 
 
 class SessionParser:
@@ -32,18 +32,77 @@ class SessionParser:
         self.path = path
         self.sites = {}
         self.session_length = session_length
-        self.cur_uid = 0
+        self.cur_uid = 1
+        self.cur_session = 1
+        self.max_siteid = 0
+        self.site_cols = []
+
+
+
+        new_session = {
+            "session_id": [0],
+            "user_id": [0],
+        }
+        for i in range(1, session_length + 1):
+            new_session["site" + str(i)] = 0
+            self.site_cols.append("site" + str(i))
+        self.res = pd.DataFrame(data=new_session)
+
+    def getids(self, sites):
+        sids = []
+        for site_name in sites.values:
+            sid = self.sites.get(site_name[0])
+            if not sid:
+                self.max_siteid += 1
+                self.sites[site_name[0]] = [self.max_siteid, 1]
+                sid = self.max_siteid
+            else:
+                self.sites[site_name[0]][1] += 1
+                sid = self.sites[site_name[0]][0]
+            sids.append(sid)
+        return sids
+
+    def process_session(self, session_df):
+        # extraction of all sites ids
+        site_ids = self.getids(session_df[["site"]])
+        new_session = {
+            "session_id": [self.cur_session],
+            "user_id": [self.cur_uid],
+        }
+        if len(site_ids) != self.session_length:
+            for i in range(0, self.session_length - len(site_ids)):
+                site_ids.append(0)
+        for col in zip(self.site_cols, site_ids):
+            new_session[col[0]] = [col[1]]
+        # incr session counter
+        self.cur_session+=1
+        new_session = pd.DataFrame(data=new_session)
+        self.res = self.res.append(new_session, ignore_index=True)
 
     def one_file(self, filename):
-        user_df = pd.read_csv(filename)
+        user_df = pd.read_csv(self.path + filename)
         rows = user_df.shape[0]
+        #сколько сессий в документе
         sessions = rows // self.session_length
-        last_s_length = rows % self.session_length
         for i in range(0, sessions):
-            session_slice =
+            beg = i * self.session_length
+            session_slice_df = user_df.iloc[beg: beg + self.session_length]
+            # обработка одной полной сессии
+            self.process_session(session_slice_df)
+        # последняя, неполная сессия
+        last_session = user_df.iloc[self.session_length * sessions:]
+        if last_session.shape[0]!=0:
+            self.process_session(last_session)
 
-for filename in os.listdir(path):
-    if filename.endswith(".csv"):
-        print("kek")
-    else:
-        continue
+    def parse_dir(self):
+        for filename in os.listdir(self.path):
+            if filename.endswith(".csv"):
+                uid = filename.split(".")[0].split("r")[1]
+                uid = int(uid)
+                self.cur_uid = uid
+                self.one_file(filename)
+
+
+parser = SessionParser(uspath, 5)
+parser.parse_dir()
+a = 1
